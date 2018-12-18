9bf041565623a62c000916445dc54ce94ee80c97
Anders Dalskov <anderspkd@fastmail.com>
2018-12-18 08:57:41 +0100

    initial commit

---
+++ passgen.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+from string import (ascii_lowercase, ascii_uppercase, ascii_letters,
+                    digits, punctuation)
+from sys import argv
+from secrets import choice
+
+alias = {
+    'l' : ascii_lowercase,
+    'u' : ascii_uppercase,
+    'd' : digits,
+    'p' : punctuation,
+    'a' : ascii_letters
+}
+
+def usage(progname="passgen.py"):
+    print(f'{progname} [length] [alphabet]')
+    print()
+    print(f'''Alphabet is specificed as follows:
+
+    l: {alias["l"]}
+    u: {alias["u"]}
+    d: {alias["d"]}
+    p: {alias["p"]}
+    a: {alias["a"]}
+    c: <custom alphabet>
+
+Examples,
+
+    $ {progname} 10 cabcd
+    ... # 10 character long password picked from {{a,b,c,d}}
+
+    $ {progname} 5 pa
+    ... # 5 charactor long password picked from "a" and "p" alphabets above.''')
+
+
+args = argv[1:]
+progname = argv[0]
+
+if len(args) < 2:
+    usage(progname)
+    exit(0)
+
+try:
+    ln = int(args[0])
+    if ln == 0:
+        raise ValueError
+except ValueError:
+    print(f'"{args[0]}" is not a valid length')
+
+spec = args[1]
+alph = set()
+if spec[0] == 'c':
+    # cannot use `set` here as we want to be able to introduce biases
+    alph = spec[1:]
+else:
+    for c in spec:
+        try:
+            alph.update(alias[c])
+        except KeyError:
+            print(f'ignoring unknown alphabet spec: {c}')
+    alph = ''.join(c for c in alph)
+
+if not len(alph):
+    print(f'empty alphabet for spec={spec}')
+
+pwd = ''.join(choice(alph) for _ in range(ln))
+print(pwd)
